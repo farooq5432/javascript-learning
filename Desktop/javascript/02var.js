47fca305c87a5219553e29bc714dde3035e8c612
Farooq <amazingsun810@gmail.com>
2026-06-22 00:12:25 +0500

    first commit

---
+++ Desktop/javascript/02var.js
@@ -0,0 +1,10 @@
+// intro to variables
+
+// variable can store information 
+// we can us and update it later 
+var firstname = "Farooq";
+console.log(firstname);
+
+firstname = "Aminkhan";
+
+console.log(firstname)
