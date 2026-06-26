d98f8dc25350132ce8ab8053c25ae451653f15c5
Farooq <amazingsun810@gmail.com>
2026-06-26 22:31:42 +0500

    day5

--- Desktop/javascript/44.js
+++ Desktop/javascript/44.js
@@ -10,3 +10,37 @@ function maths(x,y){
 }
 const answer = maths(40,60);
 console.log(answer)
+
+function Iseven(num){
+    if(num%2===0){
+        return true
+    } else {
+        return false
+    }
+}
+const x = Iseven(25);
+console.log(x)
+
+function findtarget(array, target){
+    for(i=0; i<=array.length; i++){
+        if(array[i]==target){
+            return i;
+        }
+    } return 0;
+}
+const arr = [2,3,4,5,6,7,8]
+const z = findtarget(arr,9)
+console.log(z)
+
+// function expression 
+
+const singing = function(){
+    console.log("hbd to farooq")
+}
+singing();
+
+const nim = function(num1,num2){
+    return num1 + num2;
+}
+const ans = nim(23,45);
+console.log(ans)
---
+++ Desktop/javascript/47.js
@@ -0,0 +1,10 @@
+// hoisting in js
+
+hello();
+function hello(){
+    console.log('hello world')
+}
+
+console.log(hell);
+var hell = "helllo";
+console.log(hell)
